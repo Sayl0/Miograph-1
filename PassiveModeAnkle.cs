d61cc30204d3ad6c99ba41435cafacea14ac1b77
Sayl0 <kapiztiromen@gmail.com>
2023-06-19 10:41:01 +0500

    +

---
+++ PassiveModeAnkle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Miograph
+{
+    public partial class PassiveModeAnkle : Form
+    {
+        public PassiveModeAnkle()
+        {
+            InitializeComponent();
+        }
+    }
+}
